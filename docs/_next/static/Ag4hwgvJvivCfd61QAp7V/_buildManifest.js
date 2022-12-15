ab7bea3f06af1fb3f9ac8c7846808e1955dce5d9
rtspw <richard.cx.tang@gmail.com>
2022-12-14 20:09:44 -0800

    Test build of web page

---
+++ docs/_next/static/Ag4hwgvJvivCfd61QAp7V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/60-352db0975d8b8949.js","static/css/ea21a72d1034c302.css","static/chunks/pages/index-17bffff6b0881a8d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
