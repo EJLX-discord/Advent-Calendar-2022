5edd464e2717d5fed90ea03ae0a6a9189a61cb0a
rtspw <richard.cx.tang@gmail.com>
2023-01-02 12:49:46 -0800

    Populate avatars and user metadata for all remaining entries

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -19,16 +19,6 @@ export default function UserProfile({ profileInfo }: UserProfileProps) {
   })()
   return (
     <div className={styles['container']}>
-      <div className={styles['image-container']}>
-        <Image
-          key={profileInfo.id}
-          src={imageSrc}
-          width="64"
-          height="64"
-          alt={profileInfo.alt ?? `Profile image for user ${profileInfo.nickname}`}
-          className={styles['image']}
-        />
-      </div>
       {profileInfo.hasServerIcon && (
         <div className={styles['image-container']}>
           <Image
@@ -41,6 +31,16 @@ export default function UserProfile({ profileInfo }: UserProfileProps) {
           />
         </div>
       )}
+      <div className={styles['image-container']}>
+        <Image
+          key={profileInfo.id}
+          src={imageSrc}
+          width="64"
+          height="64"
+          alt={profileInfo.alt ?? `Profile image for user ${profileInfo.nickname}`}
+          className={styles['image']}
+        />
+      </div>
       <div className={styles['name-container']}>
         <div className={styles['nickname']}>{profileInfo.nickname}</div>
         <div className={styles['username']}>{fullName}</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -146,6 +146,7 @@ async function getEntriesFromDir(dirName: string): Promise<DiscordEntryStore> {
     const entryInfo = await getEntryInfo(filePath)
     entries[entryInfo.id.toString()] = entryInfo
   }))
+  console.log(entries)
   return entries
 }
 
